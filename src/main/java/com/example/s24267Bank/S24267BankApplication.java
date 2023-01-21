824ea2639e124f8b44de8f9e643e5b449906cfb7
s24267 <s24267@pjwstk.edu.pl>
2023-01-21 12:12:50 +0100

    Initial

---
+++ src/main/java/com/example/s24267Bank/S24267BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s24267Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S24267BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S24267BankApplication.class, args);
+	}
+
+}
